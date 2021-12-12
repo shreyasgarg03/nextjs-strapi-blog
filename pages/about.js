a532b6b68ce03c587a4f95bc3421a7a60b9c410f
Shubham Kashyap <hex.media.1800@gmail.com>
2021-12-12 12:43:34 +0530

    pages and routing - frontend

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Page!!</h1>
+      <p>This is an app to find the dj and other musical events.</p>
+    </div>
+  )
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function SlugPage() {
+  return (
+    <div>
+      <h1>My Event!!</h1>
+    </div>
+  )
+}
